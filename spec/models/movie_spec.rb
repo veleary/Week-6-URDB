bd48b2cb39e81d4f2ed9fe91fd99432164e544fd
veleary <veleary@gmail.com>
2013-10-10 14:25:42 -0500

    update with 2 passing tests for description

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -1,5 +1,22 @@
 require 'spec_helper'
 
 describe Movie do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:movie) { Movie.new }
+
+  describe "#snippet" do
+    context "when description is less than or equal to 50 characters" do
+      it "returns the full description" do
+        movie.stub(:description).and_return("When a Roman general is betrayed and his family is")
+        allow(movie).to receive(:description).and_return("When a Roman general is betrayed and his family is")
+        expect(movie.snippet).to eq("When a Roman general is betrayed and his family is") 
+      end
+    end
+    context "when description is greater than 50 characters" do
+      it "returns the first 47 characters and appends '...' " do 
+        movie.stub(:description).and_return("When a Roman general is betrayed and his family murdered by an emperor's corrupt son, he comes to Rome as a gladiator to seek revenge.")
+        allow(movie).to receive(:description).and_return("When a Roman general is betrayed and his family...")
+        expect(movie.snippet).to eq("When a Roman general is betrayed and his family...")
+      end
+    end
+  end
 end
